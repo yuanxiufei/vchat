8376b0d64f838cb32592cf77ca4b929bb3c22085
ReginYuan <2496943860@qq.com>
2025-03-23 18:54:33 +0800

    feat: 添加vue插件

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your Electron application.</p>
+</template>
+
+<script setup lang="ts">
+  console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+</script>
+
+<style scoped>
+
+</style>
